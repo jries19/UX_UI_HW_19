bc49d74951b4f56dfad8f7f2226eaf31bebc9c02
jries19 <150613200+jries19@users.noreply.github.com>
2024-04-13 13:52:49 -0600

    added hover interaction to social media logos

--- js/index.js
+++ js/index.js
@@ -1,6 +1,19 @@
-//button interactions
+//main button interactions
 $(".mainButton").hover(function(){
     $(this).css("background-color", "#98D698");
     }, function(){
     $(this).css("background-color", "#7AAC7A");
+  });
+
+  //social media logo interactions
+  $(".emailLogo").hover(function(){
+    $(this).css("box-shadow", "0 4px 4px 0 rgba(0, 0, 0, 0.2)");
+    }, function(){
+    $(this).css("box-shadow", "none");
+  });
+
+  $(".linkedinLogo").hover(function(){
+    $(this).css("box-shadow", "0 4px 4px 0 rgba(0, 0, 0, 0.2)");
+    }, function(){
+    $(this).css("box-shadow", "none");
   });
