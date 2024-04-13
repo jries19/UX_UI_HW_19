7dc1caeeffadc6b38d05dfffed2dc33e941e8f5c
jries19 <150613200+jries19@users.noreply.github.com>
2024-04-13 12:38:32 -0600

    added js file and main button hover interactions

---
+++ js/index.js
@@ -0,0 +1,6 @@
+//button interactions
+$(".mainButton").hover(function(){
+    $(this).css("background-color", "#98D698");
+    }, function(){
+    $(this).css("background-color", "#7AAC7A");
+  });
